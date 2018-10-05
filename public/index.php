58dd76747259fb1fd716ebc59fb88a8b774af52c
prolojic <ooobpm@yandex.ru>
2018-10-05 11:48:34 +0300

    removeed all not necessary

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<?php
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Mongo;
+
+// Определяем некоторые константы с абсолютными путями
+// для использования с локальными ресурасами
+//---------------------------------------------------------
+define('BASE_PATH', __DIR__ . '/..');
+define('APP_PATH', BASE_PATH . '/app');
+//---------------------------------------------------------
+
+
+// Регистрируем автозагрузчик
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . '/controllers/',
+        APP_PATH . '/models/',
+    ]
+);
+
+$loader->register();
+
+// Создаём контейнер DI
+$di = new FactoryDefault();
+
+// астраиваем компонент представлений
+$di->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+// Setup a base URI
+$di->set(
+    'url',
+    function () {
+        $url = new UrlProvider();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
